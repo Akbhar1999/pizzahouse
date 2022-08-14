6625f5ef034f9d1bb215c94f3703e22cd1f5af5d
Akbhar1999 <106991949+Akbhar1999@users.noreply.github.com>
2022-08-14 14:40:53 +0100

    Add files via upload
    fix issue where if there is no default category colour from the categories table, a default colour is assigned to each category from the database

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,101 +1,109 @@
-const formatMoney = (price) => new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(price);
+// async wait return error: https://javascript.tutorialink.com/i-keep-getting-undefined-after-async-call-in-another-function/
+// https://contactmentor.com/find-object-in-array-of-objects/
+
+const formatMoney = (price) =>
+    new Intl.NumberFormat("en-GB", {
+        style: "currency",
+        currency: "GBP",
+    }).format(price);
 // const getCategories = () => $.get($('#categoryRoute').val(), (category) => category);
-const getColours = _ =>[
-    'primary',
-    'secondary',
-    'success',
-    'danger',
-    'warning',
-    'info',
-    'light',
-    'dark',
+
+const getColours = (_) => [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
 ];
 
-const randomColour = items => items[Math.floor(Math.random() * items.length)];
+const randomColour = (items) => items[Math.floor(Math.random() * items.length)];
 
 const handleError = (response) => {
     const errorStatus = response.status === 404;
-    if (errorStatus) console.warn('could not find the specified text file location');
+    if (errorStatus)
+        console.warn("could not find the specified text file location");
     return errorStatus;
 };
+const products = [];
+
+function getCategories() {
+    $(document).ready(function () {
+        $.ajax({
+            async: false,
+            url: $("#categoryRoute").val(),
+            success: (data) => {
+
+                const colours = getColours().slice(0, data.length);
+
+                data.forEach((item, i) =>
+                    products.push({
+                        name: item.name,
+                        colour: colours[i],
+                    })
+                );
+
+                return products;
+            },
+            error: () =>
+                console.error("There was an error fetching the categories"),
+        });
+    });
+}
+
+getCategories();
 
 
-const getCategories = async () => {
+const defaultCategoryColours = (item) => {
+    const tempColours = [];
+    const itemCategories = item.category_list.split(",");
 
-    try {
-        const url = $('#categoryRoute').val();
-        const response = await fetch(url);
-        if(handleError(response)) return;
-        return await response.json();
-    } catch (e) {
-        console.error(`There was an error fetching text file ${e.message}`);
-    }
+    // manually populate category colours
+
+    // itemCategories.forEach(category => {
+    //     switch (category) {
+    //         case "pizza":
+    //             tempColours.push("danger");
+    //             break;
+    //         case "crusts":
+    //             tempColours.push("info");
+    //             break;
+    //         case "toppings":
+    //             tempColours.push("success");
+    //             break;
+    //     }
+
+    // });
+    itemCategories.forEach(category => tempColours.push(products.find((product) => product.name == category).colour))
+    return tempColours;
 };
 
-// const allCategories = async () => {
-//     let productDetails = [];
-//     getCategories().then(categories =>
-//         categories.forEach(category => productDetails.push({
-//             categoryName: category.name,
-//             colour: randomColour(getColours())
-//         })))
-//     return productDetails
-
-// }
-const allCategories = () => {
-    const productDetails = [];
-    const categoryList = getCategories();
-    const random = () => randomColour(getColours())
-
-    categoryList.then(categories => {
-        for (let i = 0; i < categories.length; i++) {
-            const category = categories[i];
-            productDetails.push({
-                name: category.name,
-                colour: random()
-            })
-
-        }
-
-        console.log(productDetails)
-        return productDetails;
-    })
-}
-
-
-
-const DEFAULT_CATEGORIES = allCategories();
-
-const defaultCategoryColours = _ => getColours();
-
-const defaultCategoryColours2 = item => {
-    
-}
-
-
-const renderUI = (categories, colours) =>{
-    let output = '';
-    categories.forEach((category, i) => output += `<span class="badge text-bg-${colours[i] ?? 'warning'} ${i % 2 ? 'm-1' : ''}">${category}</span>`);
+const renderUI = (categories, colours = getColours()) => {
+    let output = "";
+    categories.forEach(
+        (category, i) =>
+        (output += `<span class="badge text-bg-${colours[i] ?? "warning"} ${i % 2 ? "m-1" : ""
+            }">${category}</span>`)
+    );
     return output;
-}
-
+};
 
 const showCategories = (item) => {
-    // console.log({ item })
-    let categories = item.category_list.split(',');
-    let coloursExists = item.category_colours
-    let colours = item.category_colours ? item.category_colours.split(',') : defaultCategoryColours();
-    if (!coloursExists) colours = defaultCategoryColours();
-    if (coloursExists && colours.length != categories.length) colours = defaultCategoryColours()
-   
+    const defaultColour = _ => colours = defaultCategoryColours(item);
+    const categories = item.category_list.split(",");
+    const coloursExists = item.category_colours;
+    let colours = coloursExists && coloursExists.length != categories.length ? item.category_colours.split(",") : '';
 
+    if (!coloursExists) defaultColour()
+    if (coloursExists && colours.length != categories.length) defaultColour()
 
-    return renderUI(categories, colours)
-};
+    return renderUI(categories, colours);
 
+}
 
-
-const setMessage = (message, type = 'alert-danger') => `
+const setMessage = (message, type = "alert-danger") => `
 <div class="alert ${type} mt-4" role="alert">
     ${message}
 </div>
